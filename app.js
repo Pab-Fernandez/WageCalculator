a806cf88879540125ecf888246db738e9466a715
Pab-Fernandez <pablower@gmail.com>
2023-05-11 03:10:05 -0300

    fn instead of const

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //function horas trabajadas.
-const hourlyWage = 10;
-const hoursWorked = 40;
-const dolarfurro = 468;
+const hourlyWage = () => 10;
+const hoursWorked = () => 40;
+const dolarfurro = () => 468;
 
 const billedTime = () => {
   return hourlyWage * hoursWorked;
@@ -10,34 +10,34 @@ console.log(billedTime());
 
 //a pesitos
 const makeThePesosHitTheFloor = () => {
-  return billedTime () * dolarfurro;
+  return billedTime() * dolarfurro();
 };
 console.log(makeThePesosHitTheFloor());
 
 //random AFIP
 const randomZeroOrOne = () => {
   return Math.round(Math.random());
-}
+};
 
-let sliceAfip;
-if (randomZeroOrOne () === 0) {
-  sliceAfip = 5;
-} else {
-  sliceAfip = 7;
-}
-
-console.log (sliceAfip)
+const sliceAfip = () => {
+  if (randomZeroOrOne() === 0) {
+    sliceAfip = 5;
+  } else {
+    sliceAfip = 7;
+  }
+};
+console.log(sliceAfip());
 // afip life
 
-const tax = (sliceAfip * 0.01)
+const tax = () => sliceAfip() * 0.01;
 const pesosLessAfip = () => {
-    return makeThePesosHitTheFloor () - (makeThePesosHitTheFloor () * tax)
-}
+  return makeThePesosHitTheFloor() - makeThePesosHitTheFloor() * tax();
+};
 
-console.log (pesosLessAfip())
+console.log(pesosLessAfip());
 //the  taxing k
-const toxicTax = 0.035
+const toxicTax = () => 0.035;
 const pesosWithtax = () => {
-    return makeThePesosHitTheFloor() - (makeThePesosHitTheFloor() * toxicTax)
-}
-console.log (pesosWithtax())
+  return makeThePesosHitTheFloor() -(makeThePesosHitTheFloor() * toxicTax());
+};
+console.log(pesosWithtax());
